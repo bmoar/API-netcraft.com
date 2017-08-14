7bcfd4bc8016f1e48b7019116cf8abf88e045714
bmoar <ben@rooted.systems>
2017-08-14 12:16:48 -0700

    update script for cli usage and fix broken things

--- NetcraftAPI.py
+++ NetcraftAPI.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 This is the (unofficial) Python API for netcraft.com Website.
 Using this code, you can retrieve subdomains
@@ -11,43 +12,31 @@ import urllib
 import math
 
 
-class NetcraftAPI(object):
-
+class NetcraftAPI():
     """
         NetcraftAPI Main Handler
     """
 
-    _instance = None
-    _verbose = False
-
-    def __init__(self, arg=None):
-        pass
-
-    def __new__(cls, *args, **kwargs):
-        """
-            __new__ builtin
-        """
-        if not cls._instance:
-            cls._instance = super(NetcraftAPI, cls).__new__(
-                cls, *args, **kwargs)
-            if (args and args[0] and args[0]['verbose']):
-                cls._verbose = True
-        return cls._instance
+    def __init__(self, verbose=False):
+        self.verbose = verbose
 
     def display_message(self, s):
-        if (self._verbose):
-            print '[verbose] %s' % s
+        if (self.verbose):
+            print('[verbose] %s' % s)
 
     def search(self, domain):
         res = []
 
         url = "http://searchdns.netcraft.com/?restriction=site+contains&host=*.%s&lookup=wait..&position=limited" % domain
+        headers = {
+            'User-Agent': 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0'
+        }
         s = requests.session()
-        req = s.get(url)
-        soup = BeautifulSoup(req.content)
+        req = s.get(url, headers=headers)
+        soup = BeautifulSoup(req.content, "lxml")
 
         pattern = 'Found (\d+) site'
-        number_results = re.findall(pattern, req.content)
+        number_results = re.findall(pattern, req.content.decode('utf-8'))
 
         if (len(number_results) > 0 and number_results[0] != '0'):
             number_results = int(number_results[0])
@@ -55,15 +44,15 @@ class NetcraftAPI(object):
             number_pages = int(math.ceil(number_results / 20)) + 1
 
             pattern = 'rel="nofollow">([a-z\.\-A-Z0-9]+)<FONT COLOR="#ff0000">'
-            subdomains = re.findall(pattern, req.content)
+            subdomains = re.findall(pattern, req.content.decode('utf-8'))
             res.extend(subdomains)
             last_result = subdomains[-1]
 
-            for index_page in xrange(1, number_pages):
+            for index_page in range(1, number_pages):
                 url = "http://searchdns.netcraft.com/?host=*.%s&last=%s.%s&from=%s&restriction=site contains&position=limited" % (domain, last_result, domain, (index_page * 20 + 1))
-                req = s.get(url)
+                req = s.get(url, headers=headers)
                 pattern = 'rel="nofollow">([a-z\-\.A-Z0-9]+)<FONT COLOR="#ff0000">'
-                subdomains = re.findall(pattern, req.content)
+                subdomains = re.findall(pattern, req.content.decode('utf-8'))
                 res.extend(subdomains)
                 for subdomain in subdomains:
                     self.display_message('[!] Found: %s ' % subdomain)
@@ -72,3 +61,11 @@ class NetcraftAPI(object):
         else:
             self.display_message("No results found for %s" % domain)
             return res
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        sys.exit("NetcraftAPI.py DOMAIN")
+    else:
+        for i in NetcraftAPI().search(sys.argv[1]):
+            print(i)
